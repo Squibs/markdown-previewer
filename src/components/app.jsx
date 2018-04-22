a613a2cff19dceab74503ad875c745b25cb5c461
Squibs <z.r.holman+github@gmail.com>
2018-04-21 19:17:55 -0600

    Removed/changed default boilerplate files.

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
 import { hot } from 'react-hot-loader';
 
-import BoilerplateInfo from './boilerplate_info';
-
 /* eslint-disable react/prefer-stateless-function */
 
 class App extends Component {
   render() {
     return (
       <div>
-        <BoilerplateInfo />
+        Markdown Previewer
       </div>
     );
   }
